471632cedb0b63fef48b17afb4b87037e8704449
kaocher <kaocher82@gmail.com>
2018-12-25 05:56:50 +0600

    update gender

--- web/app1/models.py
+++ web/app1/models.py
@@ -6,5 +6,10 @@ class retust(models.Model):
     name = models.CharField(max_length=50)
     age = models.IntegerField()
     class_of_student = models.IntegerField()
+    Gender = (
+        ('male', 'boys'),
+        ('female', 'girls'),
+    )
+    gender = models.CharField(max_length=10, choices=Gender)
     def __str__(self):
-        return f"My name is {self.name} My age is {self.age} and I am read in class {self.class_of_student}"
+        return f"My name is {self.name} My age is {self.age} and I am read in class {self.class_of_student} i am a {self.gender}"
